a35679a60891f201aa1da8c4111c2c181a125410
Matei Nicolae <nmatei@sdl.com>
2022-12-06 19:20:04 +0200

    prepare to present selected verse. improve css

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 function cleaunUp() {
   // remove all notes
-  document.querySelectorAll(".note.f").forEach(n => {
+  document.querySelectorAll(".note").forEach(n => {
     n.innerHTML = "";
     n.className = "";
   });
@@ -11,11 +11,108 @@ function cleaunUp() {
   });
 }
 
+function selectVersesToProjectInitEvents() {
+  const showProject = false;
+
+  function printSelectedVerses(tab) {
+    const selectedVerses = [...document.querySelectorAll(".verse.selected")];
+
+    tab.document.body.innerHTML = selectedVerses.map(v => `<p>${v.innerText}</p>`).join("\n");
+  }
+
+  if (!window.selectVersesToProject) {
+    let projectTab;
+    console.warn("window.selectVersesToProject");
+    window.selectVersesToProject = function (e) {
+      if (e.target.matches(".verse .label")) {
+        const multiSelect = e.ctrlKey;
+        const verse = e.target.closest(".verse");
+        const cls = "." + verse.className.split(/\s+/).join(".");
+        console.warn("click on verse number", cls);
+        const verses = document.querySelectorAll(cls);
+
+        const selected = verse.classList.contains("selected");
+        console.warn("multiSelect", multiSelect, "selected", selected);
+
+        if (!multiSelect) {
+          document.querySelectorAll(".verse.selected").forEach(v => {
+            v.classList.remove("selected");
+          });
+        }
+
+        if (!selected) {
+          verses.forEach(v => {
+            v.classList.toggle("selected");
+          });
+        }
+
+        //verse.scrollIntoView(false);
+
+        if (showProject) {
+          if (!projectTab) {
+            projectTab = window.open("", "_blank", "toolbar=no,location=no,directories=no,status=no,menubar=no");
+            const styles = `
+            body {
+              background: #000;
+              color: #fff;
+            }
+          `;
+            const styleSheet = projectTab.document.createElement("style");
+            styleSheet.id = "bible-projector";
+            styleSheet.innerText = styles;
+            projectTab.document.head.appendChild(styleSheet);
+          }
+
+          printSelectedVerses(projectTab);
+        }
+      }
+    };
+  }
+
+  function addBodyStyles() {
+    if (document.querySelector("#bible-projector")) {
+      return;
+    }
+    // TODO more space (left - right)
+    const styles = `
+    .row, body.c_pages.a_privacy .privacy_content {
+      max-width: 90rem;
+    }
+    .bible-reader-arrows-container {
+      max-width: 100%;
+    }
+    .verse .label {
+      cursor: pointer;
+      padding: 5px 5px 5px 8px;
+      border-radius: 4px;
+    }
+    .verse.selected .label {
+      background: #000;
+      color: #fff;
+    }
+  `;
+    const styleSheet = document.createElement("style");
+    styleSheet.id = "bible-projector";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+  }
+
+  addBodyStyles();
+
+  document.querySelector("#react-app-Bible").removeEventListener("click", selectVersesToProject);
+  document.querySelector("#react-app-Bible").addEventListener("click", selectVersesToProject);
+}
+
 chrome.action.onClicked.addListener(tab => {
   if (tab.url.startsWith("https://my.bible.com/bible")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: cleaunUp
+    });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: selectVersesToProjectInitEvents
     });
   } else {
     chrome.tabs.create({ url: "https://my.bible.com/bible" });
